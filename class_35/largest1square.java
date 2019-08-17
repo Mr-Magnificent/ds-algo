879e84ad714630cd3683153ab88ae71b1f0a4f2c
Ayush Poddar <ayush_poddar@live.com>
2019-08-17 15:10:06 +0530

    class 36

--- class_35/largest1square.java
+++ class_35/largest1square.java
@@ -3,17 +3,10 @@ import java.util.*;
 public class largest1square {
 
     public static int largestSquare(int[][] mat) {
-        
+
         int[][] strg = new int[mat.length][mat[0].length];
-        for (int i = 0; i < mat.length; i++) {
-            for(int j = 0; j < mat[0].length; j++) {
-                if (mat[i][j] == 0) {
-                    strg[i][j] = 0;
-                }
-            }
-        }
-
-
+        
+        return strg[0][0];
     }
     public static void main(String[] args) {
         int[][] arr = {
